a32ff017c138d82f6d9bec6c3bda0c21b60f9edc
Mohammad <orabe.mhd@gmail.com>
2024-06-02 12:03:56 +0200

    add super and vererbung

---
+++ src/live_coding/kapitel2_OOP_vertiefung/Super4.java
@@ -0,0 +1,92 @@
+package live_coding.kapitel2_OOP_vertiefung;
+/* Super
+* 1. Mit Variablen
+* 2. Mit Methoden
+* 3. Mit Konstruktoren
+* */
+
+
+class Fahrzeug{
+    int maxSpeed = 60;
+    String name = "Reem";
+
+    Fahrzeug(){
+        System.out.println("Das ist ein leeres Konstruktor von Fahrzeug." + this.maxSpeed + " " + this.name);
+    }
+
+    Fahrzeug(String name){
+        this.name = name;
+        System.out.println("Das ist Konstruktor mit 1 params von Fahrzeug." + this.maxSpeed + " " + this.name);
+    }
+
+    Fahrzeug(int maxSpeed, String name){
+        this.maxSpeed = maxSpeed;
+        this.name = name;
+        System.out.println("Das ist Konstruktor mit 2 params von Fahrzeug." + this.maxSpeed + " " + this.name);
+    }
+
+    void message(){
+        System.out.println("Das ist die Fahrzeug-klasse");
+    }
+
+}
+
+class Pkw extends Fahrzeug{
+    int maxSpeed = 180;
+
+    Pkw(){
+        System.out.println("Das ist ein leeres Konstruktor von Pkw." + this.maxSpeed + " " + this.name);
+    }
+
+    Pkw(int maxSpeed){
+        super();
+        this.maxSpeed = maxSpeed;
+        System.out.println("Das ist ein Konstruktor mit einem Param von Pkw." + this.maxSpeed + " " + this.name);
+    }
+
+    Pkw(int maxSpeed, String name){
+        super(name);
+        this.maxSpeed = maxSpeed;
+        System.out.println("Das ist ein Konstruktor mit einem Param von Pkw." + this.maxSpeed + " " + this.name);
+    }
+
+    void message(){
+        System.out.println("Das ist die Pkw-klasse");
+    }
+
+    void display(){
+//        System.out.println(maxSpeed); // shortcut. aehnlich wie mit this.maxSpeed
+//        System.out.println(this.maxSpeed); // this. ist ueberfluessig
+//        System.out.println(super.maxSpeed); // 60
+//        System.out.println(super.name); // reem
+
+//        message(); // shortcut. aehnlich wie mit this.maxSpeed
+//        this.message(); // this. ist ueberfluessig
+//        super.message();
+    }
+
+}
+
+public class Super4 {
+    public static void main(String[] args) {
+        Fahrzeug fahrzeug1 = new Fahrzeug();
+        Fahrzeug fahrzeug2 = new Fahrzeug(250, "mercedes");
+
+        Pkw pkw0 = new Pkw(); // maxSpeed = 180, (name ist reem)
+        Pkw pkw1 = new Pkw(200, "VW"); //maxSpeed = 200, (name ist  VW)
+        Pkw pkw2 = new Pkw(300); //maxSpeed = 300, (name ist reem)
+
+//        System.out.println(pkw1.maxSpeed); // 180
+//
+//        System.out.println("-----------");
+//
+//        pkw0.display(); // 180, 180, 60. reem
+//        System.out.println("***");
+//
+//        pkw1.display(); // 200, 200, 60, reem
+//        System.out.println("***");
+//
+//        pkw2.display(); // 300, 300, 60, reem
+
+    }
+}
---
+++ src/live_coding/kapitel2_OOP_vertiefung/Wiederholung3_vererbung.java
@@ -0,0 +1,76 @@
+package live_coding.kapitel2_OOP_vertiefung;
+
+class Kleidung{
+    int quali;
+    double preis = 10;
+    static String firmenName;
+
+    Kleidung(){
+        this(10, 24.99, "New Yorker");
+        System.out.println("das ist ein leeres Konstruktor von Kleidung");
+    }
+
+    Kleidung(int quali, double preis){
+//        this.quali = quali;
+//        this.preis = preis;
+        this(quali, preis, "Adidas");
+        System.out.println("das ist ein Konstruktor von Kleidung mit 2 Args");
+    }
+
+    Kleidung(int quali, double preis, String firmenName){
+        this.quali = quali;
+        this.preis = preis;
+        this.firmenName = firmenName;
+        System.out.println("Das ist ein Konstrukor von Kleidung mit 3 args");
+    }
+
+    void printSomething(){
+        System.out.println("Heute ist Freitag");
+    }
+
+    public String toString(){
+        return "Quali: " + quali + ", Preis: " + preis + ", FirmName: " + firmenName;
+    }
+}
+
+class Hose extends Kleidung{
+    int quali;
+    double preis = 20;
+    String size;
+
+    Hose(){
+        System.out.println("das ist ein leeres Konstruktor von Hose");
+    }
+
+    Hose(int quali, double preis, String size){
+        this.quali = quali;
+        this.preis = preis;
+        this.size = size;
+        System.out.println("das ist ein Konstruktor von Hose mit 2 args");
+    }
+
+    void printSomething(){
+        System.out.println("Heute ist Freitag");
+    }
+
+    public String toString(){
+        return "Quali: " + quali + ", Preis: " + preis + ", size: " + size + ", firemName: " + firmenName;
+    }
+
+}
+
+public class Wiederholung3_vererbung {
+    public static void main(String[] args) {
+        Kleidung kleidung1 = new Kleidung(); //
+        Kleidung kleidung2 = new Kleidung(11, 25.99, "New Yorker");
+
+//        System.out.println(kleidung1); // Quali: 0, Preis: 10.0, FirmName: null
+//        System.out.println(kleidung2); // Quali: 10, Preis: 24.99, FirmName: null
+//
+//        Hose hose1 = new Hose();
+//        Hose hose2 = new Hose(4, 99.99, "XXL");
+//        System.out.println(hose1); // Quali: 0, Preis: 20.0, size: null, FirmName: New Yorker
+//        System.out.println(hose2); // Quali: 4, Preis: 99.99, size: XXL, FirmName: New Yorker
+
+    }
+}
